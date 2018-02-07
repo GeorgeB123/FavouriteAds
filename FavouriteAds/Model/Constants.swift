6104e960d0b4962b7069a52e7b30c8564ff87835
George Bonnici-Carter <george.bonnici@zoocha.com>
2018-02-07 09:05:00 +0000

    Updating constraints

--- FavouriteAds/Model/Constants.swift
+++ FavouriteAds/Model/Constants.swift
@@ -17,7 +17,7 @@ struct Constants {
     static let krone = ",-"
     static let urlForAdsJson = "https://gist.githubusercontent.com/3lvis/3799feea005ed49942dcb56386ecec2b/raw/63249144485884d279d55f4f3907e37098f55c74/discover.json"
     static let baseImageUrl = "https://images.finncdn.no/dynamic/480x360c/"
-    static let cellEdgeConstraints = CGFloat(3)
+    static let cellEdgeConstraints = CGFloat(2.5)
     static let cornerRadius = CGFloat(10)
     static let favouriteHeart = #imageLiteral(resourceName: "fillHeart")
     static let emptyHeart = #imageLiteral(resourceName: "clearHeart")
